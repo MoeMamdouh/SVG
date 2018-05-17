78d26b6e5cfa9787377e68774b6e5e4262603e26
Moe-Mamdouh <mohamed.mamdouh.do@gmail.com>
2018-05-18 01:32:39 +0200

    add SVG cow component

--- App.js
+++ App.js
@@ -1,38 +1,33 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
+import { Platform, StyleSheet, View, Dimensions } from 'react-native';
+import Svg, {
+  Circle,
+  Ellipse,
+  G,
+  LinearGradient,
+  RadialGradient,
+  Line,
+  Path,
+  Polygon,
+  Polyline,
+  Rect,
+  Symbol,
   Text,
-  View
-} from 'react-native';
+  Use,
+  Defs,
+  Stop,
+} from 'react-native-svg';
+import Cow from './src/components/CowSVG';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const { width, height } = Dimensions.get('screen');
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class SvgExample extends Component {
+  static title = 'Tap the shapes to change state';
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Cow />
       </View>
     );
   }
@@ -41,18 +36,21 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+    // paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
   },
-  welcome: {
-    fontSize: 20,
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
     textAlign: 'center',
-    margin: 10,
+    color: '#34495e',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  border: {
+    borderRadius: 0,
+    borderWidth: 1,
+    borderColor: '#000000',
   },
 });
